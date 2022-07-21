43a22457edaf9d5ddf9d825674d33aa4d55682a2
soransh <57911107+soransh-singh@users.noreply.github.com>
2022-07-21 14:45:52 +0530

    update

---
+++ DHT11/DHT11.ino
@@ -0,0 +1,25 @@
+#include "DHT.h"
+#define DHTTYPE DHT11
+#define dht_dpin 11
+
+DHT dht(dht_pin, DHTTYPE);
+
+void setup() {
+  dht.begin();
+  Serial.begin(9600);
+  Serial.println("Humidity and Temperature \n\n");
+  Serial.println("=============================");
+  delay(700);
+}
+
+void loop() {
+  float h = dht.readHumidity();
+  float t = dht.readTemperature();
+  Serial.print("Current Humidity= ");
+  Serial.print(h);
+  Serial.println("% ");
+  Serial.print("Current Temperature= ");
+  Serial.print(t);
+  Serial.println("C ");
+  delay(800);
+}
--- ultraSonic/ultraSonic.ino
+++ ultraSonic/ultraSonic.ino
@@ -1,11 +1,33 @@
 const int trigPin = 9;
-const int echoPin = 10; 
+const int echoPin = 10;
+const int ledPin = 13;
+long duration;
+int distance;
 void setup() {
   // put your setup code here, to run once:
-  
+  pinMode(trigPin, OUTPUT);
+  pinMode(echoPin, INPUT);
+  pinMode(ledPin, OUTPUT);
+  Serial.begin(9600);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-
+  digitalWrite(ledPin, LOW);
+  digitalWrite(trigPin, LOW);
+  delayMicroseconds(2);
+  digitalWrite(trigPin, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trigPin, LOW);
+  duration = pulseIn(echoPin, HIGH);
+  distance = duration*0.034/2;
+  if(distance >= 200 || distance <= 0){
+    Serial.print("Out of Range");
+     
+    }else{
+    Serial.print("DISTANCE: ");
+    Serial.println(distance);
+    digitalWrite(ledPin, HIGH);
+    delay(100);  
+  }
 }
